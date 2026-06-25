01d6749fd2b78f84dd6335730374d51c39aac71e
cileenantari <cileenayman@outlook.com>
2026-06-26 01:57:16 +0300

    commit 54

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
@@ -25,6 +26,9 @@ Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
   if (Firebase.apps.isEmpty) {
     await Firebase.initializeApp(
       options: DefaultFirebaseOptions.currentPlatform,
+    );
+    await FirebaseAuth.instance.setSettings(
+      appVerificationDisabledForTesting: true,
     );
   }
   debugPrint(
